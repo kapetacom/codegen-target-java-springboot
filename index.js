5c133e2740263bdff2c0e2790b992b8bc0c08735
Henrik von Hofmeister <hh@cloudscrape.com>
2019-09-28 12:36:27 +0200

    DEXI-505: Added per-db configuration
    Adds support for per-resource configuration where the database name
    is provided by the cluster service

--- index.js
+++ index.js
@@ -122,4 +122,11 @@ class Java8SpringBoot2Target extends Target {
 
 }
 
+Target.TYPES['POSTGRES_CLIENT'] = 'postgres_client';
+Target.TYPES['MONGODB_CLIENT'] = 'mongodb_client';
+
+Target.TEMPLATES['sqldb.blockware.com/v1/postgresql'] = Target.TYPES.POSTGRES_CLIENT;
+Target.TEMPLATES['nosql.blockware.com/v1/mongodb'] = Target.TYPES.MONGODB_CLIENT;
+
+
 module.exports = Java8SpringBoot2Target;
