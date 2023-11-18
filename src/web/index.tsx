87eab22093bb2b77e24106ba1ffa82944fee213a
Henrik Hofmeister <henrik@newdawn.dk>
2023-11-18 11:03:24 +0100

    feat: Add supported resource kinds (#23)

--- src/web/index.tsx
+++ src/web/index.tsx
@@ -67,6 +67,13 @@ const targetConfig: ILanguageTargetProvider<JavaTargetConfigOptions> = {
     version: packageJson.version,
     title: kapetaDefinition.metadata.title,
     blockKinds: ['kapeta/block-type-service'],
+    resourceKinds: [
+        'kapeta/resource-type-mongodb',
+        'kapeta/resource-type-postgresql',
+        'kapeta/resource-type-rest-api',
+        'kapeta/resource-type-rest-client',
+        'kapeta/resource-type-smtp-client',
+    ],
     definition: kapetaDefinition,
     editorComponent: JavaTargetConfig,
     validate: (options: any) => {
