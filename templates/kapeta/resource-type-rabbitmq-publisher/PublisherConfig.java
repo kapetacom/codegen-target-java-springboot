96c89a5f6fd62f5efa6f873b02583355aba7dd01
Henrik Hofmeister <henrik@newdawn.dk>
2024-02-06 14:36:57 +0100

    fix: Also be specific about provider payload class

--- templates/kapeta/resource-type-rabbitmq-publisher/PublisherConfig.java
+++ templates/kapeta/resource-type-rabbitmq-publisher/PublisherConfig.java
@@ -8,13 +8,14 @@ import com.kapeta.spring.rabbitmq.RabbitConnectionManager;
 import com.kapeta.spring.rabbitmq.RabbitMQProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import {{packageName options.basePackage}}.dto.{{class data.spec.payloadType.type}};
 
 @Configuration
 public class {{class data.metadata.name type=true}}PublisherConfig {
 
     @Bean
-    public static RabbitMQProvider {{camelCase data.metadata.name}}Provider(RabbitConnectionManager rabbitConnectionManager) {
-        return new RabbitMQProvider(rabbitConnectionManager, "{{string data.metadata.name}}");
+    public static RabbitMQProvider<{{class data.spec.payloadType.type}}> {{camelCase data.metadata.name}}Provider(RabbitConnectionManager rabbitConnectionManager) {
+        return new RabbitMQProvider<>(rabbitConnectionManager, "{{string data.metadata.name}}", {{class data.spec.payloadType.type}}.class);
     }
 
 }
