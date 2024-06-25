c1488067a9422169d1c643424c808f941e5dac96
Morten Holm <mohoholm@gmail.com>
2024-06-25 07:34:12 +0200

    fix: add missing test files

---
+++ test/resources/examples/users/src/main/java/org/mycompany/services/todo/config/BaseConfig.java
@@ -0,0 +1,10 @@
+package org.mycompany.services.todo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Base configurations
+ * */
+@Configuration
+public class BaseConfig {}
